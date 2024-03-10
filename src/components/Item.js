6d79598505a2ba93af709526b3ac1feda73d0b45
Abrar <hossainabrar@aol.com>
2024-03-10 17:52:25 -0400

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [cartState, toggleCart] = useState(true)
+
+  function handleCart(){
+    toggleCart(()=> !cartState)
+  }
+  const cart = cartState ? "" : "in-cart"
+  
+  const color = cartState ? "yellow" : "Fuchsia"
+
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart} style={{background: color}}>{cartState ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
